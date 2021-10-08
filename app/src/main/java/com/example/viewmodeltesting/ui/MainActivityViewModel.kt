726c401d6a963a72ca134d9be8922046c24f7c8e
Valeriy Lyutyy <lutyj.v.ogl@gmail.com>
2021-10-08 09:23:36 +0300

    loader added

--- app/src/main/java/com/example/viewmodeltesting/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/example/viewmodeltesting/ui/MainActivityViewModel.kt
@@ -1,23 +1,27 @@
 package com.example.viewmodeltesting.ui
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import com.example.viewmodeltesting.model.RandomUserResponse
 import com.example.viewmodeltesting.network.ApiInterface
 import com.example.viewmodeltesting.network.RetrofitService
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 class MainActivityViewModel(context: Application) : AndroidViewModel(context) {
 
-    private val _message = MediatorLiveData<String>()
     private var apiInterface: ApiInterface = RetrofitService.getInterface()
 
+    private val _message = MutableLiveData<String>()
     val message: LiveData<String>
         get() = _message
+
+    private val _isFetching = MutableLiveData<Boolean>()
+    val isFetching: LiveData<Boolean>
+        get() = _isFetching
 
     fun updateTextView(newMessage: String) {
         _message.value = newMessage
@@ -39,6 +43,8 @@ class MainActivityViewModel(context: Application) : AndroidViewModel(context) {
 
         apiInterface
             .getRandomUser()
+            .doOnSubscribe { showProgressBar(true) }
+            .doAfterTerminate { showProgressBar(false) }
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe (this::showNewValue) { throwable -> showError(throwable) }
@@ -50,5 +56,9 @@ class MainActivityViewModel(context: Application) : AndroidViewModel(context) {
 
     private fun showError(throwable: Throwable) {
         _message.value = throwable.message ?: "error"
+    }
+
+    private fun showProgressBar(isShowing: Boolean) {
+        _isFetching.postValue(isShowing)
     }
 }
